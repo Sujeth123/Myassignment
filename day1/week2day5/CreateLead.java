5cf89c61bada63213e7ae50a392a9591d6903f24
Sujeth123 <94662588+Sujeth123@users.noreply.github.com>
2026-02-13 20:20:08 +0530

    Add files via upload

---
+++ day1/week2day5/CreateLead.java
@@ -0,0 +1,31 @@
+package week2day5;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CreateLead {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ChromeDriver driver=new ChromeDriver();
+driver.get("http://leaftaps.com/opentaps/");
+driver.manage().window().maximize();
+driver.findElement(By.id("username")).sendKeys("DemoCSR2");
+driver.findElement(By.id("password")).sendKeys("crmsfa");
+driver.findElement(By.className("decorativeSubmit")).click();
+driver.findElement(By.linkText("CRM/SFA")).click();
+driver.findElement(By.linkText("Leads")).click();
+driver.findElement(By.linkText("Create Lead")).click();
+driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Sujeth");
+driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Maha");
+driver.findElement(By.id("createLeadForm_companyName")).sendKeys("kidszee");
+driver.findElement(By.name("generalProfTitle")).sendKeys("Create Lead");
+driver.findElement(By.name("submitButton")).click();
+String pageName=driver.getTitle();
+System.out.println("The Page title is :" + pageName);
+driver.close();
+
+
+	}
+
+}
